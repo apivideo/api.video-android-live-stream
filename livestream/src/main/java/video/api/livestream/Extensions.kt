8a1ecdfc80afe0c6a44345ea061d80930d27fcc1
ThibaultBee <thibault@api.video>
2021-11-22 10:01:12 +0100

    fix(LiveStream): add a trailing slash at the end of an URL if it is missing

---
+++ livestream/src/main/java/video/api/livestream/Extensions.kt
@@ -0,0 +1,10 @@
+package video.api.livestream
+
+/**
+ * Add a slash at the end of a [String] only if it is missing.
+ *
+ * @return the given string with a trailing slash.
+ */
+fun String.addTrailingSlashIfNeeded(): String {
+    return if (this.endsWith("/")) this else "$this/"
+}
