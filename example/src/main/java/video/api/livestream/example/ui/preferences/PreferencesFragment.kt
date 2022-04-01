77ab1b9a0446fc7c74c08027e8918114a48e463b
ThibaultBee <thibault@api.video>
2022-04-01 15:43:11 +0200

    feat(helper): add a mechanism to check supported configuration

--- example/src/main/java/video/api/livestream/example/ui/preferences/PreferencesFragment.kt
+++ example/src/main/java/video/api/livestream/example/ui/preferences/PreferencesFragment.kt
@@ -3,6 +3,7 @@ package video.api.livestream.example.ui.preferences
 import android.os.Bundle
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import video.api.livestream.ConfigurationHelper
 import video.api.livestream.app.R
 import video.api.livestream.enums.Resolution
 
@@ -17,7 +18,7 @@ class PreferencesFragment : PreferenceFragmentCompat() {
     }
 
     private fun inflatesPreferences() {
-        (findPreference(getString(R.string.video_resolution_key)) as ListPreference?)?.apply {
+        (findPreference(getString(R.string.video_resolution_key)) as ListPreference?)!!.apply {
             val resolutionsList = Resolution.values().map { it.toString() }.toTypedArray()
             entryValues = resolutionsList
             entries = resolutionsList
@@ -25,6 +26,30 @@ class PreferencesFragment : PreferenceFragmentCompat() {
                 value = Resolution.RESOLUTION_720.toString()
             }
         }
-    }
 
+        (findPreference(getString(R.string.video_fps_key)) as ListPreference?)!!.apply {
+            val supportedFramerates = ConfigurationHelper.video.getSupportedFrameRates(
+                requireContext(),
+                "0"
+            )
+            entryValues.filter { fps ->
+                supportedFramerates.any { it.contains(fps.toString().toInt()) }
+            }.toTypedArray().run {
+                this@apply.entries = this
+                this@apply.entryValues = this
+            }
+        }
+
+        (findPreference(getString(R.string.audio_sample_rate_key)) as ListPreference?)!!.apply {
+            val supportedSampleRate =
+                ConfigurationHelper.audio.getSupportedSampleRates()
+            entries =
+                supportedSampleRate.map { "${"%.1f".format(it.toString().toFloat() / 1000)} kHz" }
+                    .toTypedArray()
+            entryValues = supportedSampleRate.map { "$it" }.toTypedArray()
+            if (entry == null) {
+                value = "44100"
+            }
+        }
+    }
 }
