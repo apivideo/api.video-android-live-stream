3fec3c7fafe7e7514e0940eb452ad214a3e3aacb
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-02-20 18:08:56 +0100

    fix(lib): fix 480p resolution

--- livestream/src/main/java/video/api/livestream/enums/Resolution.kt
+++ livestream/src/main/java/video/api/livestream/enums/Resolution.kt
@@ -8,7 +8,7 @@ import android.util.Size
 enum class Resolution(val size: Size) {
     RESOLUTION_240(Size(352, 240)),
     RESOLUTION_360(Size(640, 360)),
-    RESOLUTION_480(Size(858, 480)),
+    RESOLUTION_480(Size(854, 480)),
     RESOLUTION_720(Size(1280, 720)),
     RESOLUTION_1080(Size(1920, 1080));
 
